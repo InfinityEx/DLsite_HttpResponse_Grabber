4fb6a1f7d51af27e12402cc70247e86f771e88f4
時無_ShiWu <756423078@qq.com>
2023-01-03 23:19:12 +0800

    create news_more.py

---
+++ dragalia_news_more.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Author:时无ShiWu
+#Filename:dragalia_news_more.py
+#Version:1.0
+
+import requests as rqs
+import random
+import time
+import os
+import urllib.parse as uri
+import pandas as pd
+
+# Running path
+path=os.path.split(os.path.realpath(__file__))[0]
