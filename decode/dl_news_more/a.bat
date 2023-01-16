c8003aeac6deae97f2108d083fd281fb7939f95c
時無_ShiWu <756423078@qq.com>
2023-01-16 20:19:57 +0800

    dragalia_news_more.py bug fixed

--- decode/dl_news_more/a.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-md en_eu
-md en_us
-md ja_jp
-md zh_cn
-md zh_tw
-pause *.*
--- dragalia_news_more.py
+++ dragalia_news_more.py
@@ -32,9 +32,20 @@ def json2file(recont,rejson):
     if djson['data']['category']['priority_lower_than']==None:
         plt=-1
     else:
+        # save as json
+        with open(f"{path}/origin/dl_news_more/{lang}/{priority_m}.json",'w',encoding='utf-8') as nl:
+            nl.write(str(contents).replace("\/","/"))
+            nl.close()
+        with open(f"{path}/decodejson/dl_news_more/{lang}/{priority_m}.json",'w',encoding='utf-8') as kl:
+            ktx=bytes(contents).decode('unicode_escape').replace("\/","/")
+            kl.write(str(ktx))
+            kl.close()
+
         plt=djson['data']['category']['priority_lower_than']
         jslen=len(djson['data']['category']['contents'])
         jsdata=djson['data']['category']['contents']
+        print(f'content length:{jslen}')
+        time.sleep(2)
         for a in range (0,jslen):
             article_id=jsdata[a]['article_id']
             priority=jsdata[a]['priority']
@@ -49,14 +60,6 @@ def json2file(recont,rejson):
             is_update=jsdata[a]['is_update']
             update_time=jsdata[a]['update_time']
 
-            # save data
-            with open(f"{path}/origin/dl_news_more/{lang}/{priority_m}.json",'w',encoding='utf-8') as nl:
-                nl.write(str(contents).replace("\/","/"))
-                nl.close()
-            with open(f"{path}/decodejson/dl_news_more/{lang}/{priority_m}.json",'w',encoding='utf-8') as kl:
-                ktx=bytes(contents).decode('unicode_escape').replace("\/","/")
-                kl.write(str(ktx))
-                kl.close()
             with open(f"{path}/decode/dl_news_more/{lang}/{priority_m}.txt",'a+',encoding='utf-8') as nt:
                 nt.write(f"contents_order:{a}\narticle_id:{article_id}\npriority:{priority}\ncategory_name:{category_name}\npr_category_id:{pr_category_id}\ncaption_type:{caption_type}\npr_thumb_type:{pr_thumb_type}\ntitle_name:{title_name}\nimage_path:{image_path}\ndate:{date}\nis_new:{is_new}\nis_update:{is_update}\nupdate_time:{update_time}\n\n")
                 nt.close()
@@ -64,7 +67,8 @@ def json2file(recont,rejson):
                 if image_path!="":
                     pic.write(f"{image_path}\n")
                 pic.close()
-            print('File saved. lang:{lang}, priority:{priority_m}')
+
+        print(f'File saved. lang:{lang}, priority:{priority_m}')
     return plt
 
 if __name__=='__main__':
