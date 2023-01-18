8cb9a267f22637bb524a23f3159d59f3228ebceb
時無_ShiWu <756423078@qq.com>
2023-01-18 22:34:16 +0800

    fixed bug, update dlnews_more

--- dragalia_news_more.py
+++ dragalia_news_more.py
@@ -15,13 +15,13 @@ import pandas as pd
 path=os.path.split(os.path.realpath(__file__))[0]
 
 # languages
-languages=['zh_cn','zh_tw','en_us','en-eu','ja_jp']
+languages=['zh_cn','zh_tw','en_us','en_eu','ja_jp']
 
 # timedelta
 td=uri.quote('+08:00')
 
 # fake UA headers
-headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
+headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.52'}
 
 def json2file(recont,rejson):
     # contents: original response data
@@ -29,8 +29,13 @@ def json2file(recont,rejson):
     contents=recont
     djson=rejson
     # plt means priority_lower_than
-    if djson['data']['category']['priority_lower_than']==None:
+    try:
+        plta=djson['data']['category']['priority_lower_than']
+    except KeyError:
+        plta=-1
+    if plta==None:
         plt=-1
+        print(f'End of {lang}')
     else:
         # save as json
         with open(f"{path}/origin/dl_news_more/{lang}/{priority_m}.json",'w',encoding='utf-8') as nl:
@@ -41,9 +46,9 @@ def json2file(recont,rejson):
             kl.write(str(ktx))
             kl.close()
 
-        plt=djson['data']['category']['priority_lower_than']
-        jslen=len(djson['data']['category']['contents'])
-        jsdata=djson['data']['category']['contents']
+        plt=rejson['data']['category']['priority_lower_than']
+        jslen=len(rejson['data']['category']['contents'])
+        jsdata=rejson['data']['category']['contents']
         print(f'content length:{jslen}')
         for a in range (0,jslen):
             article_id=jsdata[a]['article_id']
@@ -71,19 +76,25 @@ def json2file(recont,rejson):
     return plt
 
 if __name__=='__main__':
-    # webpage address
-    priority_m=3742
-    for a in range(1,5):
+    # default setting:(0,5)
+    for a in range(3,4):
         lang=languages[a]
-        while(priority_m!=-1):
+        print(lang)
+        # webpage address
+        priority_m=3742
+        while True:
             if priority_m==3742:
                 # &priority_lower_than= is equivalent to &priority_lower_than=3742
                 mainhp=f"https://dragalialost.com/api/index.php?format=json&type=information&category_id=&priority_lower_than=&action=information_list&article_id=&lang={lang}&td={td}"
                 response=rqs.post(url=mainhp,headers=headers)
                 defrt=json2file(response.content,response.json())
-                priority_m=defrt
+                priority_m=int(defrt)
+                time.sleep(random.randrange(0,1))
+            elif priority_m==-1:
+                break
             else:
                 mainhp=f"https://dragalialost.com/api/index.php?format=json&type=information&category_id=&priority_lower_than={priority_m}&action=information_list&article_id=&lang={lang}&td={td}"
                 response=rqs.post(url=mainhp,headers=headers)
                 defrt=json2file(response.content,response.json())
-                priority_m=defrt
+                priority_m=int(defrt)
+                time.sleep(random.randrange(0,1))
