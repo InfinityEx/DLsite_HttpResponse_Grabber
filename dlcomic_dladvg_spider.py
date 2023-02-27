7e470dd82805b5a665a8d4f0423ffc608607c92d
InfinityEx <756423078@qq.com>
2023-02-27 17:06:07 +0800

    update dladvg.py pre-test code

--- dlcomic_dladvg_spider.py
+++ dlcomic_dladvg_spider.py
@@ -1,28 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#Author:时无ShiWu
-#Filename:dlcomic_dladvg_spider.py
-#Version:1.0
+# Author:时无ShiWu
+# Filename:dlcomic_dladvg_spider.py
+# Version:1.0
 
 import requests as rqs
 import random
-import time,os
+import time, os
 import pandas as pd
 
 # Running path
-path=os.path.split(os.path.realpath(__file__))[0]
+path = os.path.split(os.path.realpath(__file__))[0]
 
 # languages
 # lang:chs, cht, en, en-gb, jp
-lang=['chs','cht','en','en-gb','jp']
+lang = ['chs', 'cht', 'en', 'en-gb', 'jp']
 # type(jp):ゆるがりあ, ドラガリのはじめかた！, ナームのわくわく冒険記
 # type(chs):轻松龙约, 失落的龙约上手方法！, 娜姆的波澜冒险记
-type=['dragalialife','adventurersguide','plotsynopsis']
+type = ['dragalialife', 'adventurersguide', 'plotsynopsis']
 
-headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/84.0.4147.105 Safari/537.36'}
+
+clist=[]
 
 # adventurersguide
 # parts: index, detail
+def index(lang, ctype):
+    data = {'lang': lang, 'type': ctype}
+    iresponse = rqs.post(url=f'https://comic.dragalialost.com/api/index', headers=headers, data=data)
+    ijson = iresponse.json()
 
-if __name__=='__main__':
-    print()
+    with open(f'{path}/origin/comic_dladvg/{lang}/index.json') as idx:
+        idx.write(iresponse.content)
+        idx.close()
+
+    idata = pd.json_normalize(ijson['items'][0])
+
+    # item in json
+    pid = idata['id']
+    pepnum = idata['episode_num']
+    ptitle = idata['title']
+    pmain = idata['main']
+    pts = idata['thumbnail_s']
+    ptl = idata['thumbnail_l']
+    plen = len(idata)
+
+
+
+if __name__ == '__main__':
+    # for a in lang:
+    a = lang[0]
+    print(a, type[1])
+    index(a, type[1])
