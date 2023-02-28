f60c37621dbb22f4bb86396118cd6a6e61689f99
InfinityEx <756423078@qq.com>
2023-02-28 13:37:14 +0800

    update dladvg.py, upload json&res_list

--- dlcomic_dladvg_spider.py
+++ dlcomic_dladvg_spider.py
@@ -23,7 +23,8 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/84.0.4147.105 Safari/537.36'}
 
-clist=[]
+clist = []
+
 
 # adventurersguide
 # parts: index, detail
@@ -32,35 +33,60 @@ def index(lang, ctype):
     iresponse = rqs.post(url=f'https://comic.dragalialost.com/api/index', headers=headers, data=data)
     ijson = iresponse.json()
 
-    idlen=len(ijson['items'])
-    for x in idlen:
-        clist[x]=ijson['items'][x]['id']
+    idlen = len(ijson['items'])
+    for x in range(0, idlen):
+        clist.append(int(ijson['items'][x]['id']))
 
-    with open(f'{path}/origin/comic_dladvg/{lang}/index.json') as idx:
-        idx.write(iresponse.content)
+        # item in json
+        idata = ijson['items'][x]
+        pmain = idata['main']
+        pts = idata['thumbnail_s']
+        ptl = idata['thumbnail_l']
+
+        with open(f'{path}/res_list/comic_dladvg/dlcomic_advg_{lang}_main.txt', 'a+', encoding='utf-8') as ma:
+            ma.write(f'{pmain}\n')
+            ma.close()
+        with open(f'{path}/res_list/comic_dladvg/dlcomic_advg_{lang}_thumbnail_s.txt', 'a+', encoding='utf-8') as ths:
+            ths.write(f'{pts}\n')
+            ths.close()
+        with open(f'{path}/res_list/comic_dladvg/dlcomic_advg_{lang}_thumbnail_l.txt', 'a+', encoding='utf-8') as thl:
+            thl.write(f'{ptl}\n')
+            thl.close()
+
+    with open(f'{path}/origin/comic_dladvg/{lang}/index.json', 'w', encoding='utf-8') as idx:
+        idx.write(str(iresponse.content))
         idx.close()
 
-    with open(f'{path}/origin/comic_dladvg/{lang}/index.json') as ids:
-        dect=bytes(iresponse.content).decode('unicode_escape').replace("\/","/")
+    with open(f'{path}/decodejson/comic_dladvg/{lang}/index.json', 'w', encoding='utf-8') as ids:
+        dect = bytes(iresponse.content).decode('unicode_escape').replace("\/", "/")
         ids.write(str(dect))
         ids.close()
 
-    # item in json
-    # pid = idata['id']
-    # pepnum = idata['episode_num']
-    # ptitle = idata['title']
-    # pmain = idata['main']
-    # pts = idata['thumbnail_s']
-    # ptl = idata['thumbnail_l']
-    # plen = len(idata)
+    time.sleep(random.randint(0, 2))
+    print(f'{lang} index json saved.')
 
-def detail(lang,ctype,cid):
-    print('')
+
+def detail(lang, ctype, cid):
+    # print(f'{lang},{ctype},{cid}')
+    data = {'lang': lang, 'type': ctype}
+    dresponse = rqs.post(url=f'https://comic.dragalialost.com/api/detail/{cid}', headers=headers, data=data)
+
+    with open(f'{path}/origin/comic_dladvg/{lang}/{cid}.json', 'w', encoding='utf-8') as ddt:
+        ddt.write(str(dresponse.content))
+        ddt.close()
+
+    with open(f'{path}/decodejson/comic_dladvg/{lang}/{cid}.json', 'w', encoding='utf-8') as dds:
+        decd = bytes(dresponse.content).decode('unicode_escape').replace("\/", "/")
+        dds.write(str(decd))
+        dds.close()
+
+    time.sleep(random.randint(0, 2))
+
 
 if __name__ == '__main__':
-    # for a in lang:
-    a = lang[0]
-    print(a, type[1])
-    index(a, type[1])
-    for b in clist:
-        detail(a,type[1],clist[b])
+    for a in lang:
+        index(a, type[1])
+        for b in range(0, len(clist)):
+            detail(a, type[1], clist[b])
+        clist.clear()
+        print(f'{a} details saved.')
